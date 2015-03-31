f6eca37acb073ddfedd857ca5a9e63f36555d49d
Joshua K. Farrar <joshua@sent1nel.me>
2015-03-31 15:33:21 -0400

    prevent the static asset server from caching

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require 'webrick'
 
+class NonCachingFileHandler < WEBrick::HTTPServlet::FileHandler
+  def prevent_caching(res)
+    res['ETag']          = nil
+    res['Last-Modified'] = Time.now + 100**4
+    res['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0'
+    res['Pragma']        = 'no-cache'
+    res['Expires']       = Time.now - 100**4
+  end
+
+  def do_GET(req, res)
+    super
+    prevent_caching(res)
+  end
+end
+
 server = WEBrick::HTTPServer.new :Port => 3000
-server.mount '/', WEBrick::HTTPServlet::FileHandler, './client'
+server.mount '/', NonCachingFileHandler, './client'
 
 trap('INT') { server.stop }
 
