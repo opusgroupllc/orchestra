c02f00599458e49570e09e57341240321bfd8dbb
Joshua K. Farrar <joshua@sent1nel.me>
2015-04-06 18:16:04 -0400

    update serialization of user models

--- server/app/controllers/user.rb
+++ server/app/controllers/user.rb
@@ -5,6 +5,10 @@ Orchestra::App.controllers :user do
     ActiveModel::ArraySerializer.new(User.all, each_serializer: UserSerializer).to_json
   end
 
+  get :show, :map => '/api/v1/users/:id' do
+    UserSerializer.new(User.find(params[:id]), root: false).to_json
+  end
+
   post :session, :map => '/api/v1/users/:username/sessions' do
     user = User.find_by_email(params[:username])
 
@@ -14,5 +18,4 @@ Orchestra::App.controllers :user do
       error 400, { :error => "Invalid credentials." }.to_json
     end
   end
-
 end
